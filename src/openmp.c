17b3fa57668df9a300d3c37f8f009c879156f66e
Pramod Raghunath Potdar <prpotdar1@sheffield.ac.uk>
2022-05-20 16:28:38 +0100

    open mp stage 2 perfected and all 3 stages working great

--- src/openmp.c
+++ src/openmp.c
@@ -18,7 +18,8 @@ void openmp_begin(const Image *input_image) {
 
     // Allocate buffer for calculating the sum of each tile mosaic
     omp_mosaic_sum = (unsigned long long*)malloc(omp_TILES_X * omp_TILES_Y * input_image->channels * sizeof(unsigned long long));
-
+    // Set values initially to 0
+    memset(omp_mosaic_sum, 0, omp_TILES_X * omp_TILES_Y * omp_input_image.channels * sizeof(unsigned long long));
     // Allocate buffer for storing the output pixel value of each tile
     omp_mosaic_value = (unsigned char*)malloc(omp_TILES_X * omp_TILES_Y * input_image->channels * sizeof(unsigned char));
     
@@ -32,42 +33,25 @@ void openmp_begin(const Image *input_image) {
     omp_output_image.data = (unsigned char *)malloc(input_image->width * input_image->height * input_image->channels * sizeof(unsigned char));
 }
 void openmp_stage1() {
-    // Reset sum memory to 0
-    memset(omp_mosaic_sum, 0, omp_TILES_X * omp_TILES_Y * omp_input_image.channels * sizeof(unsigned long long));
-    // Sum pixel data within each tile
-//#pragma omp for
-//#pragma omp parallel for
-// 
-    omp_set_nested(1);
+    // declare required variables
     int t_x, t_y, p_x, p_y, tile_index, tile_offset, pixel_offset;
-    #pragma omp parallel for shared(omp_mosaic_sum) private(t_x, t_y, p_x, p_y, tile_index, tile_offset, pixel_offset) // reduction(+: omp_mosaic_sum)
+    #pragma omp parallel for shared(omp_mosaic_sum) private(t_x, t_y, p_x, p_y, tile_index, tile_offset, pixel_offset)
     for (t_x = 0; t_x < omp_TILES_X; ++t_x) {
-        //#pragma omp parallel for shared(omp_mosaic_sum) private(t_x, t_y, p_x, p_y, tile_index, tile_offset, pixel_offset)
         for (t_y = 0; t_y < omp_TILES_Y; ++t_y) {
             tile_index = (t_y * omp_TILES_X + t_x) * omp_input_image.channels;
             tile_offset = (t_y * omp_TILES_X * TILE_SIZE * TILE_SIZE + t_x * TILE_SIZE) * omp_input_image.channels;
-            //int p_x, p_y;
-            // For each pixel within the tile
-            // #pragma omp parallel for
-            //#pragma omp parallel for shared(omp_mosaic_sum) private(t_x, t_y, p_x, p_y, tile_index, tile_offset, pixel_offset)
             for (p_x = 0; p_x < TILE_SIZE; ++p_x) {
-                //#pragma omp parallel for shared(omp_mosaic_sum)
                 for (p_y = 0; p_y < TILE_SIZE; ++p_y) {
                     // For each colour channel
                     pixel_offset = (p_y * omp_input_image.width + p_x) * omp_input_image.channels;
-                    for (int ch = 0; ch < omp_input_image.channels; ++ch) {
-                        // Load pixel
-                        const unsigned char pixel = omp_input_image.data[tile_offset + pixel_offset + ch];
-                        // #pragma omp critical
-                        omp_mosaic_sum[tile_index + ch] += pixel;
-                        
-                    }
+                    // loop unrolling 
+                    omp_mosaic_sum[tile_index + 0] += omp_input_image.data[tile_offset + pixel_offset + 0];
+                    omp_mosaic_sum[tile_index + 1] += omp_input_image.data[tile_offset + pixel_offset + 1];
+                    omp_mosaic_sum[tile_index + 2] += omp_input_image.data[tile_offset + pixel_offset + 2];
                 }
             }
         }
     }
-    // Optionally during development call the skip function with the correct inputs to skip this stage
-    // skip_tile_sum(input_image, mosaic_sum);
 
 #ifdef VALIDATION
     // TODO: Uncomment and call the validation function with the correct inputs
@@ -75,24 +59,24 @@ void openmp_stage1() {
 #endif
 }
 void openmp_stage2(unsigned char* output_global_average) {
-    // Calculate the average of each tile, and sum these to produce a whole image average.
-    unsigned long long whole_image_sum[4] = {0, 0, 0, 0};  // Only 3 is required for the assignment, but this version hypothetically supports upto 4 channels
-    //int omp_tot_TILES = omp_TILES_X * omp_TILES_Y;
-    //int t, ch;
-    //#pragma omp parallel for shared(omp_mosaic_value) private(t, ch)
-    for (unsigned int t = 0; t < omp_TILES_X * omp_TILES_Y; ++t) {
-        for (int ch = 0; ch < omp_input_image.channels; ++ch) {
-            omp_mosaic_value[t * omp_input_image.channels + ch] = (unsigned char)(omp_mosaic_sum[t * omp_input_image.channels + ch] / TILE_PIXELS);  // Integer division is fine here
-            whole_image_sum[ch] += omp_mosaic_value[t * omp_input_image.channels + ch];
+    unsigned long long r = 0, g = 0, b = 0;
+    
+    int t;
+    #pragma omp parallel for private(t) schedule(static,32) reduction(+: r, g, b) // shared(omp_mosaic_value, omp_mosaic_sum) 
+    for (t = 0; t < omp_TILES_X * omp_TILES_Y * CHANNELS; ++t) {
+        omp_mosaic_value[t] = (unsigned char)(omp_mosaic_sum[t] / TILE_PIXELS);
+        switch (t % 3) {
+        case 0: r += omp_mosaic_value[t]; break;
+        case 1: g += omp_mosaic_value[t]; break;
+        case 2: b += omp_mosaic_value[t]; break;
         }
     }
 
     // Reduce the whole image sum to whole image average for the return value
-    for (int ch = 0; ch < omp_input_image.channels; ++ch) {
-        output_global_average[ch] = (unsigned char)(whole_image_sum[ch] / (omp_TILES_X * omp_TILES_Y));
-    }
-    // Optionally during development call the skip function with the correct inputs to skip this stage
-    // skip_compact_mosaic(TILES_X, TILES_Y, mosaic_sum, compact_mosaic, global_pixel_average);
+    // loop unrolling
+    output_global_average[0] = (unsigned char)(r / (omp_TILES_X * omp_TILES_Y));
+    output_global_average[1] = (unsigned char)(g / (omp_TILES_X * omp_TILES_Y));
+    output_global_average[2] = (unsigned char)(b / (omp_TILES_X * omp_TILES_Y));
 
 #ifdef VALIDATION
     // TODO: Uncomment and call the validation functions with the correct inputs
@@ -102,9 +86,8 @@ void openmp_stage2(unsigned char* output_global_average) {
 void openmp_stage3() {
     // Broadcast the compact mosaic pixels back out to the full image size
     // For each tile
-
     int t_x, t_y, p_x, p_y, tile_index, tile_offset, pixel_offset;
-    #pragma omp parallel for private(t_x, t_y, p_x, p_y, tile_index, tile_offset, pixel_offset)
+    #pragma omp parallel for private(t_x, t_y, p_x, p_y, tile_index, tile_offset, pixel_offset) 
     for (t_x = 0; t_x < omp_TILES_X; ++t_x) {
         for (t_y = 0; t_y < omp_TILES_Y; ++t_y) {
             tile_index = (t_y * omp_TILES_X + t_x) * omp_input_image.channels;
@@ -120,8 +103,6 @@ void openmp_stage3() {
             }
         }
     }
-    // Optionally during development call the skip function with the correct inputs to skip this stage
-    // skip_broadcast(input_image, compact_mosaic, output_image);
 
 #ifdef VALIDATION
     // TODO: Uncomment and call the validation function with the correct inputs
